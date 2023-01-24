f572755e02fc389cdff661ded9a02c4a6c5a0ab3
aliciafdezc <cfdezalicia@gmail.com>
2023-01-24 11:03:57 +0100

    Limpieza ficheros

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -11,9 +10,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    SharedModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
