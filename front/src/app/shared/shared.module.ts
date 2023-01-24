6f5796aecad8e5b6f8f639e8b600f72bbf0640ac
aliciafdezc <cfdezalicia@gmail.com>
2023-01-24 12:17:18 +0100

    - Directiva sticky top

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
-
+import { DirectivasModule } from './directivas/directivas.module';
 
 
 @NgModule({
@@ -9,7 +9,8 @@ import { MenuComponent } from './menu/menu.component';
     MenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    DirectivasModule
   ],
   exports: [
     MenuComponent
