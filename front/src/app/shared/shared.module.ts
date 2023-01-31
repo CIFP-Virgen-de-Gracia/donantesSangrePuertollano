a9d43e19f5b49006819e1ff6ed7ae559c2ce295c
aliciafdezc <cfdezalicia@gmail.com>
2023-01-31 22:56:42 +0100

    - Componente formulario email newsletter - Animación newsletter - Estilos popup newsletter

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -1,25 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivasModule } from './directivas/directivas.module';
-import { NewsletterComponent } from './newsletter/newsletter.component';
-
+import { EmailFormularioComponent } from './newsletter/email-formulario/email-formulario.component';
+import { NewsletterVentanaComponent } from './newsletter/newsletter-ventana/newsletter-ventana.component';
 
 @NgModule({
   declarations: [
     MenuComponent,
     FooterComponent,
-    NewsletterComponent
+    EmailFormularioComponent,
+    NewsletterVentanaComponent
   ],
   imports: [
     CommonModule,
-    DirectivasModule
+    DirectivasModule,
+    ReactiveFormsModule
   ],
   exports: [
     MenuComponent,
     FooterComponent,
-    NewsletterComponent
+    NewsletterVentanaComponent
   ]
 })
 export class SharedModule { }
