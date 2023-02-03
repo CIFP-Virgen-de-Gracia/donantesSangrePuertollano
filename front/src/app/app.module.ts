d576bdb0f2d865b91e908e9fd13a3bf2d71aab3d
Alextorres20 <alex56789torres@gmail.com>
2023-02-03 19:36:31 +0100

    Test funcion terminado (Falta poner más preguntas)

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,19 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { PaginasModule } from './paginas/paginas.module';/*
+import { PaginasModule } from './paginas/paginas.module';
+import { AptoSangreComponent } from './apto-sangre/apto-sangre.component';
+import { AptoSangreModule } from './apto-sangre/apto-sangre.module';/*
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; */
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AptoSangreComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,/*
     NgbModule, */
     SharedModule,
-    PaginasModule
+    PaginasModule,
+    AptoSangreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
