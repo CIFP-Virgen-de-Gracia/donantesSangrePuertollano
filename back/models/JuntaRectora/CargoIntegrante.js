e81e4d419359390488d0f627049bf30592d3bd1c
aliciafdezc <cfdezalicia@gmail.com>
2023-02-08 10:30:25 +0100

    Recuperar y pintar junta directiva

--- back/models/JuntaRectora/CargoIntegrante.js
+++ back/models/JuntaRectora/CargoIntegrante.js
@@ -1,29 +1,28 @@
-const {DataTypes} = require("sequelize");
-const sequelize = require('../../database/ConexionSequelize');/* 
+const { DataTypes } = require("sequelize");
+/* const sequelize = require('../../database/ConexionSequelize');
 const CargoJunta = require('./CargoJunta'); */
 require('dotenv').config();
 
 //Alicia
-
-
-module.exports = (sequelize) => { sequelize.db.define("cargoIntegrante", {
-    idCargo: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        allowNull: false, 
+module.exports = (sequelize) => {
+    sequelize.db.define("cargoIntegrante", {
+        idCargo: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            allowNull: false,
+        },
+        idIntegrante: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            allowNull: false,
+        }
     },
-    idIntegrante: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        allowNull: false, 
-    }
-},
-{
-    timestamps: false
-},
-{
-    tableName: 'cargoIntegrante'
-});
+    {
+        timestamps: false
+    },
+    {
+        tableName: 'cargoIntegrante'
+    });
 }
 /* 
 CargoIntegrante.belongsTo(CargoJunta, {foreignKey: 'id'}); */
