dddd5f731e151df3fa187fd661d62e9d7e49d879
dqLaptop <isabelmm331@gmail.com>
2023-02-10 14:13:29 +0100

    Mostrar imagenes, parte back editar y borrar, añadidos estilos

--- back/controllers/noticiasController.js
+++ back/controllers/noticiasController.js
@@ -2,45 +2,48 @@ const { response, request } = require('express');
 const uploadFile = require("../middlewares/upload");
 const queriesNoticias = require("../database/queries/queriesNoticias");
 const fs = require('fs');
+const codificar = require('image-to-base64');
 const path = require('path');
 
 //Todo Isa
 const getListado = async (req, res = response) => {
     queriesNoticias.getListado(req.params.seccion).then((noticia) => {
-        let not = [];
-        noticia.forEach(n => {
-            let data;
-            let fecha = new Date(n.createdAt).toLocaleString();
-            let parrafo = n.contenido.split("\n");
-            if (n['Imagen'].length > 0) {
-                data = {
-                    "id": n.id,
-                    "titulo": n.titulo,
-                    "subtitulo": n.subtitulo,
-                    "contenido": parrafo,
-                    "fecha": fecha,
-                    "imagen": n["Imagen"][0]['nombre']
+        console.log(noticia);
+        if (noticia !== null) {
+            let not = [];
+            noticia.forEach(n => {
+                let data;
+                let fecha = new Date(n.createdAt).toLocaleString();
+                let parrafo = n.contenido.split("\n");
+                if (n['Imagen'].length > 0) {
+                    data = {
+                        "id": n.id,
+                        "titulo": n.titulo,
+                        "subtitulo": n.subtitulo,
+                        "contenido": parrafo,
+                        "fecha": fecha,
+                        "imagen": "http://127.0.0.1:8090/api/Noticias/upload/" + n.id
+                    }
+                } else {
+                    data = {
+                        "id": n.id,
+                        "titulo": n.titulo,
+                        "subtitulo": n.subtitulo,
+                        "contenido": parrafo,
+                        "fecha": fecha,
+                        "imagen": ""
+                    }
                 }
-            } else {
-                data = {
-                    "id": n.id,
-                    "titulo": n.titulo,
-                    "subtitulo": n.subtitulo,
-                    "contenido": parrafo,
-                    "fecha": fecha,
-                    "imagen": n["Imagen"]
-                }
-            }
-            not.push(data);
-        });
-        res.status(200).json(not);
+                not.push(data);
+            });
+            res.status(200).json(not);
+        }
     }).catch((err) => {
         console.log(err);
         res.status(200).json("No encontrada");
     });
 }
 const registrarNoticia = async (req, res = response) => {
-    console.log(req.files);
     queriesNoticias.insertarNoticias(req).then((noticia) => {
         res.status(200).json(noticia);
     }).catch((err) => {
@@ -51,23 +54,36 @@ const registrarNoticia = async (req, res = response) => {
 
 const getNoticia = (req, res = response) => {
     queriesNoticias.getNoticia(req.body.id).then((noticia) => {
-        res.status(200).json(noticia);
+        if (noticia !== null) {
+            res.status(200).json(noticia);
+        } else {
+            res.status(200).json("No encontrada");
+        }
     }).catch((err) => {
         res.status(200).json("No encontrada");
     });
 }
 
 const borrarNoticia = (req, res = response) => {
-    queriesNoticias.borrarNoticias(req.body.id).then((noticia) => {
+    queriesNoticias.borrarNoticia(req.body.id).then((noticia) => {
         res.status(200).json("La noticia ha sido borrada");
     }).catch((err) => {
+        console.log(err);
         res.status(200).json("No se ha podido borrar");
+    });
+}
+const modificarNoticia = (req, res = response) => {
+    queriesNoticias.modificarNoticia(req).then((noticia) => {
+        res.status(200).json(noticia);
+    }).catch((err) => {
+        console.log(err)
+        res.status(203).json("No se ha podido modificar");
     });
 }
 const mostrarImagen = (req, res = response) => {
     queriesNoticias.getImagen(req.params.id).then((imagen) => {
         if (imagen) {
-            pathImagen = path.join(__dirname, '../uploads', 'noticias', imagen.nombre);
+            const pathImagen = path.join(__dirname, '../uploads', 'noticias', imagen.nombre);
             if (fs.existsSync(pathImagen)) {
                 return res.sendFile(pathImagen)
             }
@@ -83,5 +99,6 @@ module.exports = {
     getNoticia,
     registrarNoticia,
     borrarNoticia,
+    modificarNoticia,
     mostrarImagen
 }
