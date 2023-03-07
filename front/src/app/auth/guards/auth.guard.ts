4569e53feb9e7b895cb006bc34d106042a8a7058
aliciafdezc <cfdezalicia@gmail.com>
2023-03-07 17:36:38 +0100

    Limpiar

--- front/src/app/auth/guards/auth.guard.ts
+++ front/src/app/auth/guards/auth.guard.ts
@@ -31,6 +31,5 @@ export class AuthGuard implements CanActivate, CanMatch {
       else {
         return true;
       }
-
   }
 }
--- front/src/app/gestion-contenido/main-page-contenido/main-page-contenido.component.ts
+++ front/src/app/gestion-contenido/main-page-contenido/main-page-contenido.component.ts
@@ -98,30 +98,4 @@ export class MainPageContenidoComponent implements OnInit {
       });
     }
   }
-
-}
-
-  obtenerNoticiaAEditar(event: any) {
-    let id = event.target.id;
-    this.idModificar = id.slice(1);
-    this.ContenidoService.obtenerNoticia(this.idModificar).subscribe({
-      next: data => {
-        if (data.success !==false) {
-          this.info = data.data;
-        }
-      },
-      error: error => {
-        this.qho="si";
-      }
-    });
-  }
-
-  comprobarPuedeModificar() {
-    if (this.estaRegistrado) {
-      this.AuthService.puedeModificar().subscribe(resp => {
-        this.puedeModificar = (resp) ? true : false;
-      });
-    }
-  }
-
 }
