34f0d8ebddaa693e135c37a54b24c2a4b907a769
aliciafdezc <cfdezalicia@gmail.com>
2023-04-05 17:59:59 +0200

    modelo y migration memoria

---
+++ back/migrations/20230405153143-create-memoria.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('memorias', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      anio: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      imagen: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      archivo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('memorias');
+  }
+};
